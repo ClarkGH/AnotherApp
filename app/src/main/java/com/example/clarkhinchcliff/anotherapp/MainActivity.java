8283f7a4dd5dc48d9b3ba800334635cc6eda70f1
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-02 21:19:46 -0700

    button connected to java functions -easy

--- app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.clarkhinchcliff.anotherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickEvent(View view) {
+        Log.i("Yo", "What up?");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
