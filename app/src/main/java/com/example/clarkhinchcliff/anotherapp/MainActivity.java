ee0b77d8eb789dae130dd97453baa0ec5815e54c
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-04 20:42:29 -0700

    I googled it and I figured it out on my own -Image that changes? Pleaaase

--- app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickEvent(View view) {
         EditText inputTextField = (EditText) findViewById(R.id.textField);
+        ImageView theSwitch = (ImageView) findViewById(R.id.imageView);
         Toast.makeText(getApplicationContext(), "You said: " + inputTextField.getText().toString(), Toast.LENGTH_SHORT).show();
+        theSwitch.setImageResource(R.drawable.aliensguy);
     }
 
     @Override
