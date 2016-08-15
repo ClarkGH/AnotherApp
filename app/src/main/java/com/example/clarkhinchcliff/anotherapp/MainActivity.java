701452df5aec8e0020c8769199de918e3691b192
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-15 12:59:09 -0700

    okay

--- app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/anotherapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.clarkhinchcliff.anotherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
